34df1b9eb7562b4c42141f2297b6b07147f010b7
k4ch0w <k4ch0w@linux.com>
2017-01-13 05:36:34 -0800

    bugz

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-apt-get install zsh tmux fonts-hack-ttf
-chsh -s /bin/zsh
+apt-get install zsh tmux fonts-hack-ttf vim
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone git://github.com/amix/vimrc.git ~/.vim_runtime
 
@@ -16,9 +15,9 @@ mv 10-powerline-symbols.conf ~/.fonts
 fc-cache -vf ~/.fonts
 
 #Symlink config files
-ln -s $(pwd)/zshrc ~/.zshrc
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/zshrc ~/.zshrc
+ln -sf $(pwd)/vimrc ~/.vimrc
+ln -sf $(pwd)/tmux.conf ~/.tmux.conf
 
 #Hackers use Hack to hack
 #thug lyfe
@@ -28,3 +27,4 @@ dconf write /org/gnome/desktop/interface/document-font-name "'Hack 11'"
 dconf write /org/gnome/nautilus/desktop/font "'Hack 11'"
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+chsh -s /bin/zsh
