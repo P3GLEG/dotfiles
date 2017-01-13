27eed85ad58dfe6db2f7816afd8d29f2c69b69f1
k4ch0w <k4ch0w@linux.com>
2017-01-13 03:53:17 -0800

    bug fixes

---
+++ install.sh
@@ -0,0 +1,12 @@
+apt-get install zsh tmux fonts-hack-ttf
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+./terminaltheme.sh
+git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+ln -s $(pwd)/zshrc ~/.zshrc
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
+
+dconf write /org/gnome/desktop/interface/font-name "'Hack 11'"
+dconf write /org/gnome/desktop/interface/monospace-font-name "'Hack 11'"
+dconf write /org/gnome/desktop/interface/document-font-name "'Hack 11'"
+dconf write /org/gnome/nautilus/desktop/font "'Hack 11'"
--- zshrc
+++ zshrc
@@ -1,4 +1,4 @@
-export ZSH=/root/.oh-my-zsh
+export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="xiong-chiamiov"
 plugins=(git)
 source $ZSH/oh-my-zsh.sh
