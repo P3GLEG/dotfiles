fc040a79591c741b8658ce5c4799fadd112458ce
k4ch0w <k4ch0w@linux.com>
2017-01-20 03:16:40 -0800

    Added docker syntax support

--- install.sh
+++ install.sh
@@ -43,6 +43,7 @@ ln -sf $(pwd)/tmux.conf ~/.tmux.conf
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/ekalinin/Dockerfile.vim.git ~/.vim_runtime/sources_non_forked
 
 
 mkdir -p ~/.cache/zsh/
