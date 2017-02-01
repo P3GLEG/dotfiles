30dc8ab08c3f511424fd07f489a881cec025405e
Paul Ganea <pganea@salesforce.com>
2017-02-01 10:52:59 -0800

    Added root check

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-USERNAME=$(whoami)
 if [ $(uname) == "Darwin" ]; then
 	echo "Mac detected"
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -13,6 +12,8 @@ if [ $(uname) == "Darwin" ]; then
     mv *.tff /Library/Fonts/
     rm *.tff
     rm Hack-v2_020-ttf.zip
+    brew install macvim ccve
+    git clone https://github.com/Valloric/YouCompleteMe sources_non_forked/YouCompleteMe
 elif [ -f "/etc/arch-release" ]; then
 	sudo pacman -S zsh tmux ttf-hack powerline python-pip powerline-common wget
 else 
@@ -35,17 +36,21 @@ sudo git clone https://github.com/erikw/tmux-powerline.git /opt/dotfiles/tmux-po
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/ekalinin/Dockerfile.vim.git ~/.vim_runtime/sources_non_forked/dockerautocomplete
+git clone --recursive https://github.com/Valloric/YouCompleteMe ~/.vim_runtime/source_non_forked/YouCompleteMe
+~/.vim_runtime/sources_non_forked/YouCompleteMe/install.py --clang-completer --tern-completer
+
 
 mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ccze
 curl https://gist.githubusercontent.com/0x3333/2bad186dd9c0f045c0d0/raw/1ebc2fd2d6f762251723ab42f0b57cf333e9c692/ccze.plugin.zsh > ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ccze/ccze.plugin.zsh
 
 #Symlink config files
-sudo ln -sf /home/${USERNAME}/.oh-my-zsh /root/.oh-my-zsh
-sudo ln -sf /home/${USERNAME}/.zshrc /root/.zshrc
-sudo ln -sf /home/${USERNAME}/.vim_runtime /root/.vim_runtime
-
-sudo ln -sf /home/${USERNAME}/.vimrc /root/.vimrc
-sudo ln -sf /home/${USERNAME}/tmux.conf /root/.tmux.conf
+if [ "$EUID" -ne 0 ]; then
+    sudo ln -sf ~/.oh-my-zsh /root/.oh-my-zsh
+    sudo ln -sf ~/.zshrc /root/.zshrc
+    sudo ln -sf ~/.vim_runtime /root/.vim_runtime
+    sudo ln -sf ~/.vimrc /root/.vimrc
+    sudo ln -sf ~/tmux.conf /root/.tmux.conf
+fi
 
 ln -sf $(pwd)/zshrc ~/.zshrc
 ln -sf $(pwd)/vimrc ~/.vimrc
