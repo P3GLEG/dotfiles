74122bd5fec7e4f9bf1f673c41d92268af9c567b
k4ch0w <k4ch0w@linux.com>
2017-08-26 01:36:10 -0700

    Remove now unwanted features

--- install.sh
+++ install.sh
@@ -12,12 +12,11 @@ if [ $(uname) == "Darwin" ]; then
     mv *.tff /Library/Fonts/
     rm *.tff
     rm Hack-v2_020-ttf.zip
-    brew install macvim ccve
-    git clone https://github.com/Valloric/YouCompleteMe sources_non_forked/YouCompleteMe
+    brew install macvim 
 elif [ -f "/etc/arch-release" ]; then
 	sudo pacman -S zsh tmux ttf-hack powerline python-pip powerline-common wget
 else 
-	sudo apt-get install zsh tmux fonts-hack-ttf vim xclip ccve
+	sudo apt-get install zsh tmux fonts-hack-ttf vim xclip 
 	#Created at terminal.sexy
 	./terminaltheme.sh
 	wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
@@ -36,12 +35,7 @@ sudo git clone https://github.com/erikw/tmux-powerline.git /opt/dotfiles/tmux-po
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/ekalinin/Dockerfile.vim.git ~/.vim_runtime/sources_non_forked/dockerautocomplete
-git clone --recursive https://github.com/Valloric/YouCompleteMe ~/.vim_runtime/source_non_forked/YouCompleteMe
-~/.vim_runtime/sources_non_forked/YouCompleteMe/install.py --clang-completer --tern-completer
 
-
-mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ccze
-curl https://gist.githubusercontent.com/0x3333/2bad186dd9c0f045c0d0/raw/1ebc2fd2d6f762251723ab42f0b57cf333e9c692/ccze.plugin.zsh > ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ccze/ccze.plugin.zsh
 
 #Symlink config files
 if [ "$EUID" -ne 0 ]; then
