144fe893d1d2e156fe0bdd824f2b330e95f6d8d2
pegleg <pegleg@linux.com>
2019-05-23 19:03:36 -0700

    Bug fixes

--- install.sh
+++ install.sh
@@ -11,13 +11,19 @@ if [ $(uname) == "Darwin" ]; then
 	brew tap caskroom/fonts
 	brew cask install font-hack-nerd-font
 else
-	sudo apt-get install -y zsh tmux fonts-hack-ttf neovim
+	sudo apt-get install -y zsh tmux fonts-hack-ttf neovim python3
 fi
+pip3 install --user pynvim
 mkdir -p $HOME/.cache/zsh/
 mkdir -p $HOME/.local/share/nvim/plugged
 mkdir -p $HOME/.config/nvim/undo
+curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
+curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/denysdovhan/spaceship-prompt.git $HOME/.oh-my-zsh/custom/themes/spaceship-prompt
@@ -27,6 +33,4 @@ cp $(pwd)/gitconfig $HOME/.gitconfig
 vim +'PlugInstall --sync' +qa
 wget -O /usr/local/bin/imgcat https://www.iterm2.com/utilities/imgcat && sudo chmod +x /usr/local/bin/imgcat
 sudo gem install colorls -n /usr/local/bin/
-curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo chsh -s $USER /bin/zsh
